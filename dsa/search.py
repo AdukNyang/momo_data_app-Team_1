7b1ac4c3433b3e3119715f66ec65a19653effe53
ndavid-arch <n.david@alustudent.com>
2026-05-29 14:42:41 +0200

    Now the user can pass command line arguments because of sys

--- dsa/search.py
+++ dsa/search.py
@@ -1,10 +1,13 @@
-"linear search for MOMO transaction "
-
+"""
+linear search for MOMO transaction
+"""
+import sys
 from xml_parser import load_transactions
 
 def linear_search(transactions, target_id):
     """
-    this will loop throught the transactions and find the one whose transaction id matches the target_id
+    this will loop through the transactions and find the one whose transaction id matches the target_id
+    or returns None if no match is found
     """
     for transaction in transactions:
             if transaction["transaction_id"] == target_id:
